e32c90242d84c751daf04e883b54995664cbcfaa
Temi Oketunji <toketunji@deloitte.co.uk>
2018-11-30 15:07:32 +0000

    update the directory of mock data

--- src/services/getATFListService.js
+++ src/services/getATFListService.js
@@ -5,7 +5,7 @@ const path = require('path')
 const getATFList = () => {
   return new Promise((resolve, reject) => {
     try {
-      resolve(JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../tests/resources/mock-atf.json'), 'utf8')))
+      resolve(JSON.parse(fs.readFileSync(path.resolve(__dirname, '../mocks/mock-atf.json'), 'utf8')))
     } catch (error) {
       reject(new HTTPResponseStatus(500, error.stack))
     }
