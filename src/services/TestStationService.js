31edbd1017b494dc9a6e8e8820877c37846b563a
khodder <khodder@gmail.com>
2019-05-08 16:48:31 +0100

    Add subsegment as can't add annotations to lead segment

--- src/services/TestStationService.js
+++ src/services/TestStationService.js
@@ -1,8 +1,6 @@
 const HTTPError = require('../models/HTTPError')
 const AWSXRay = require('aws-xray-sdk-core')
 
-// AWSXRay.enableManualMode()
-
 class TestStationService {
   constructor (testStationDAO) {
     this.testStationDAO = testStationDAO
@@ -12,20 +10,18 @@ class TestStationService {
     let retrievedSegment = AWSXRay.getSegment();
     if(typeof retrievedSegment === 'undefined') retrievedSegment = new AWSXRay.Segment('hello');
 
-    console.log('TEST SEGMENT QWERTY',retrievedSegment)
-    // const segment = new AWSXRay.Segment('hello')
-    retrievedSegment.addAnnotation('retrieved segment', JSON.stringify(retrievedSegment))
+    let subSeg = retrievedSegment.addNewSubsegment('getTestStationList');
 
     return this.testStationDAO.getAll()
       .then(data => {
         if (data.Count === 0) {
           throw new HTTPError(404, 'No resources match the search criteria.')
         }
-        retrievedSegment.addAnnotation("stations", JSON.stringify(data))
+        subSeg.addAnnotation("stations", JSON.stringify(data))
         return data.Items
       })
       .catch(error => {
-        retrievedSegment.addError(error);
+        subSeg.addError(error);
         if (!(error instanceof HTTPError)) {
           console.log('thrown error', error, 'END of error')
           error.statusCode = 500
