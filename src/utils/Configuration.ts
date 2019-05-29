4017a3c77a333c2edebe6c4771d8513d7e511d90
khodder <khodder@gmail.com>
2019-05-29 10:00:48 +0100

    CVSB-3330 add enum for error messages and switch configuration.js for .ts version

--- src/utils/Configuration.ts
+++ src/utils/Configuration.ts
@@ -1,96 +1,120 @@
-"use strict";
-exports.__esModule = true;
+import {Handler} from "aws-lambda";
 // @ts-ignore
-var yml = require("node-yaml");
-var HTTPMethods;
-(function (HTTPMethods) {
-    HTTPMethods["GET"] = "GET";
-    HTTPMethods["POST"] = "POST";
-    HTTPMethods["PUT"] = "PUT";
-    HTTPMethods["DELETE"] = "DELETE";
-})(HTTPMethods || (HTTPMethods = {}));
-var Configuration = /** @class */ (function () {
-    function Configuration(configPath) {
+import * as yml from "node-yaml";
+
+enum HTTPMethods {
+    GET = "GET",
+    POST = "POST",
+    PUT = "PUT",
+    DELETE = "DELETE"
+}
+
+interface IFunctionEvent {
+    name: string;
+    method: HTTPMethods;
+    path: string;
+    function: Handler;
+}
+
+class Configuration {
+
+    private static instance: Configuration;
+    private readonly config: any;
+
+    private constructor(configPath: string) {
         this.config = yml.readSync(configPath);
+
         // Replace environment variable references
-        var stringifiedConfig = JSON.stringify(this.config);
-        var envRegex = /\${(\w+\b):?(\w+\b)?}/g;
-        var matches = stringifiedConfig.match(envRegex);
+        let stringifiedConfig: string = JSON.stringify(this.config);
+        const envRegex: RegExp = /\${(\w+\b):?(\w+\b)?}/g;
+        const matches: RegExpMatchArray | null = stringifiedConfig.match(envRegex);
+
         if (matches) {
-            matches.forEach(function (match) {
+            matches.forEach((match: string) => {
                 envRegex.lastIndex = 0;
-                var captureGroups = envRegex.exec(match);
+                const captureGroups: RegExpExecArray = envRegex.exec(match) as RegExpExecArray;
+
                 // Insert the environment variable if available. If not, insert placeholder. If no placeholder, leave it as is.
                 stringifiedConfig = stringifiedConfig.replace(match, (process.env[captureGroups[1]] || captureGroups[2] || captureGroups[1]));
             });
         }
+
         this.config = JSON.parse(stringifiedConfig);
     }
+
     /**
      * Retrieves the singleton instance of Configuration
      * @returns Configuration
      */
-    Configuration.getInstance = function () {
+    public static getInstance(): Configuration {
         if (!this.instance) {
             this.instance = new Configuration("../config/config.yml");
         }
+
         return Configuration.instance;
-    };
+    }
+
     /**
      * Retrieves the entire config as an object
      * @returns any
      */
-    Configuration.prototype.getConfig = function () {
+    public getConfig(): any {
         return this.config;
-    };
+    }
+
     /**
      * Retrieves the lambda functions declared in the config
      * @returns IFunctionEvent[]
      */
-    Configuration.prototype.getFunctions = function () {
+    public getFunctions(): IFunctionEvent[] {
         if (!this.config.functions) {
             throw new Error("Functions were not defined in the config file.");
         }
-        return this.config.functions.map(function (fn) {
-            var _a = Object.entries(fn)[0], name = _a[0], params = _a[1];
-            var path = (params.proxy) ? params.path.replace("{+proxy}", params.proxy) : params.path;
+
+        return this.config.functions.map((fn: Handler) => {
+            const [name, params]: any = Object.entries(fn)[0];
+            const path: string = (params.proxy) ? params.path.replace("{+proxy}", params.proxy) : params.path;
+
             return {
-                name: name,
+                name,
                 method: params.method.toUpperCase(),
-                path: path,
-                "function": require("../functions/" + name)[name]
+                path,
+                function: require(`../functions/${name}`)[name]
             };
         });
-    };
+    }
+
     /**
      * Retrieves the DynamoDB config
      * @returns any
      */
-    Configuration.prototype.getDynamoDBConfig = function () {
+    public getDynamoDBConfig(): any {
         if (!this.config.dynamodb) {
             throw new Error("DynamoDB config is not defined in the config file.");
         }
+
         // Not defining BRANCH will default to remote
-        var env;
+        let env;
         switch (process.env.BRANCH) {
-            case "local":
-                env = "local";
-                break;
-            case "local-global":
-                env = "local-global";
-                break;
-            default:
-                env = "remote";
+        case "local":
+            env = "local";
+            break;
+        case "local-global":
+            env = "local-global";
+            break;
+        default:
+            env = "remote";
         }
+
         return this.config.dynamodb[env];
-    };
+    }
+
     /**
      * Retrieves the test number initial value
      * @returns any
      */
-    Configuration.prototype.getTestNumberInitialValue = function () {
+    public getTestNumberInitialValue(): any {
         return this.config.testNumberinitialValue;
-    };
-    return Configuration;
-}());
-exports.Configuration = Configuration;
+    }
+}
+export { Configuration, IFunctionEvent };
