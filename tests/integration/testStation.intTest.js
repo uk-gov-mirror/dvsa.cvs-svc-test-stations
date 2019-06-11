4753c9466f3a957ac71b06025a716831b9862514
khodder <khodder@gmail.com>
2019-06-11 15:10:57 +0100

    CVSB-2868 add integration test

--- tests/integration/testStation.intTest.js
+++ tests/integration/testStation.intTest.js
@@ -48,6 +48,29 @@ describe('test stations', () => {
           })
       })
 
+      it('should return the selected Station\'s details', (done) => {
+        let expectedResponse = [
+          {
+            testStationPNumber: '84-926821',
+            testStationEmails: [
+              'teststationname@dvsa.gov.uk',
+              'teststationname1@dvsa.gov.uk'
+              ],
+            testStationId: '2'
+          }
+        ]
+
+        request.get('test-stations/84-926821')
+          .end((err, res) => {
+            if (err) { expect.fail(err) }
+            expect(res.statusCode).to.equal(200)
+            expect(res.headers['access-control-allow-origin']).to.equal('*')
+            expect(res.headers['access-control-allow-credentials']).to.equal('true')
+            expect(res.body).to.deep.equal(expectedResponse)
+            done()
+          })
+      })
+
       after((done) => {
         let dataBuffer = testStationData
 
