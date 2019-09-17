7d8921bad45a083a464778a73d120d7acb992531
Arup Banerjee <arupdvsa@alanaamy.net>
2019-09-17 23:28:44 +0100

    refactored code and made tests more logical

---
+++ tests/integration/getTestStationDdUpEmpty.intTest.ts
@@ -0,0 +1,32 @@
+import supertest from "supertest";
+import { expect } from "chai";
+import { populateDatabase, emptyDatabase } from "../util/dbOperations";
+import stations from "../resources/test-stations.json";
+const url = "http://localhost:3004/";
+const request = supertest(url);
+
+
+describe("test stations", () => {
+    beforeAll((done) => {
+        emptyDatabase();
+        done();
+    });
+
+    afterAll((done) => {
+        populateDatabase();
+        done();
+    });
+
+    describe("getTestStation Db is Up", () => {
+        context("when database is empty", () => {
+            it("should return error code 404", (done) => {
+                request.get("test-stations").expect(404, done)
+                    .end((err: Error, res: any) => {
+                        done();
+                    });
+            });
+        });
+    });
+});
+
+
