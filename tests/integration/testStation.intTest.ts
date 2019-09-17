50b07ee877aafed4cc5d64538fec73ef511b20fd
Arup Banerjee <arupdvsa@alanaamy.net>
2019-09-17 23:28:44 +0100

    refactored code and made tests more logical

--- tests/integration/testStation.intTest.ts
+++ tests/integration/testStation.intTest.ts
@@ -1,30 +1,19 @@
 import supertest from "supertest";
-import {expect} from "chai";
-import {populateDatabase, emptyDatabase} from "../util/dbOperations";
+import { expect } from "chai";
+import { populateDatabase, emptyDatabase } from "../util/dbOperations";
 import stations from "../resources/test-stations.json";
 const url = "http://localhost:3004/";
 const request = supertest(url);
 
 describe("test stations", () => {
-  beforeAll(async (done) => {
-    jest.setTimeout(30000);
-    jest.resetAllMocks();
-    jest.resetModules();
-    await populateDatabase();
-    setTimeout(done,20000);
-  });
-
-  beforeEach(() => {
-    jest.setTimeout(30000);
-  });
-
-  afterEach(() => {
-    jest.setTimeout(30000);
+  beforeAll((done) => {
+    populateDatabase();
+    done();
   });
 
   describe("getTestStation", () => {
-    context("when database is populated", () => {
-      it("should return all test stations in the database", async (done) => {
+    context("when database is populated to test all test stations", () => {
+      it("should return all test stations in the database", (done) => {
         request.get("test-stations")
           .end((err: Error, res: any) => {
             if (err) { expect.fail(err); }
@@ -36,27 +25,29 @@ describe("test stations", () => {
           });
       });
 
-      it("should return the selected Station's details", async (done) => {
-        const expectedResponse = [
-          {
-            testStationPNumber: "84-926821",
-            testStationEmails: [
-              "teststationname@dvsa.gov.uk",
-              "teststationname1@dvsa.gov.uk"
-            ],
-            testStationId: "2"
-          }
-        ];
+      context("when database is populated to test selected test station", () => {
+        it("should return the selected Station's details", (done) => {
+          const expectedResponse = [
+            {
+              testStationPNumber: "84-926821",
+              testStationEmails: [
+                "teststationname@dvsa.gov.uk",
+                "teststationname1@dvsa.gov.uk"
+              ],
+              testStationId: "2"
+            }
+          ];
 
-        request.get("test-stations/84-926821")
-          .end((err, res: any) => {
-            if (err) { expect.fail(err); }
-            expect(res.statusCode).to.equal(200);
-            expect(res.headers["access-control-allow-origin"]).to.equal("*");
-            expect(res.headers["access-control-allow-credentials"]).to.equal("true");
-            expect(res.body).to.deep.equal(expectedResponse);
-            done();
-          });
+          request.get("test-stations/84-926821")
+            .end((err, res: any) => {
+              if (err) { expect.fail(err); }
+              expect(res.statusCode).to.equal(200);
+              expect(res.headers["access-control-allow-origin"]).to.equal("*");
+              expect(res.headers["access-control-allow-credentials"]).to.equal("true");
+              expect(res.body).to.deep.equal(expectedResponse);
+              done();
+            });
+        });
       });
     });
   });
--- tests/util/dbOperations.ts
+++ tests/util/dbOperations.ts
@@ -2,22 +2,15 @@ import * as fs from "fs";
 import * as path from "path";
 import {TestStationDAO} from "../../src/models/TestStationDAO";
 import {ITestStation} from "../../src/models/ITestStation";
+import { TestStationService } from "../../src/services/TestStationService";
 
-export const populateDatabase = async () => {
-    const testStationDAO = new TestStationDAO();
-    const mockBuffer = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../resources/test-stations.json"), "utf8"));
-
-    const batches = [];
-    while (mockBuffer.length > 0) {
-        batches.push(mockBuffer.splice(0, 25));
-    }
-
-    return batches.forEach(async (batch) => {
-        return await testStationDAO.createMultiple(batch);
-    });
+export const populateDatabase =  () => {
+    const testStationMockDB = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../resources/test-stations.json"), "utf8"));
+    const testStationService = new TestStationService(new TestStationDAO());
+    testStationService.insertTestStationList(testStationMockDB);
 };
 
-export const emptyDatabase = async () => {
+export const emptyDatabase = () => {
     const testStationDAO = new TestStationDAO();
     const dataBuffer = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../resources/test-stations.json"), "utf8"));
 
@@ -25,8 +18,8 @@ export const emptyDatabase = async () => {
     while (dataBuffer.length > 0) {
         batches.push(dataBuffer.splice(0, 25));
     }
-    return batches.forEach(async (batch) => {
-        return await testStationDAO.deleteMultiple(
+    return batches.forEach((batch) => {
+        return testStationDAO.deleteMultiple(
           batch.map((item: any) => {
               return item.testStationId;
           })
