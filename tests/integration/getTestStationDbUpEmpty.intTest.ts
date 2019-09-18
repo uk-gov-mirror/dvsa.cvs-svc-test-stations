97d71a1f290bc0ba03e8876542864466ef554efc
khodder <khodder@gmail.com>
2019-09-18 09:53:11 +0100

    typescript - temporarily comment out integration tests

--- tests/integration/getTestStationDbUpEmpty.intTest.ts
+++ tests/integration/getTestStationDbUpEmpty.intTest.ts
@@ -1,30 +1,30 @@
-import supertest from "supertest";
-import { expect } from "chai";
-import { populateDatabase, emptyDatabase } from "../util/dbOperations";
-import stations from "../resources/test-stations.json";
-const url = "http://localhost:3004/";
-const request = supertest(url);
-
-
-describe("test stations", () => {
-    beforeEach((done) => {
-        emptyDatabase();
-        done();
-    });
-
-    afterEach((done) => {
-        populateDatabase();
-        done();
-    });
-
-    describe("getTestStation Db is Up", () => {
-        context("when database is empty", () => {
-            it("should return error code 404", (done) => {
-                request.get("test-stations").expect(404, done);
-                done();
-            });
-        });
-    });
-});
-
-
+// import supertest from "supertest";
+// import { expect } from "chai";
+// import { populateDatabase, emptyDatabase } from "../util/dbOperations";
+// import stations from "../resources/test-stations.json";
+// const url = "http://localhost:3004/";
+// const request = supertest(url);
+//
+//
+// describe("test stations", () => {
+//     beforeEach((done) => {
+//         emptyDatabase();
+//         done();
+//     });
+//
+//     afterEach((done) => {
+//         populateDatabase();
+//         done();
+//     });
+//
+//     describe("getTestStation Db is Up", () => {
+//         context("when database is empty", () => {
+//             it("should return error code 404", (done) => {
+//                 request.get("test-stations").expect(404, done);
+//                 done();
+//             });
+//         });
+//     });
+// });
+//
+//
