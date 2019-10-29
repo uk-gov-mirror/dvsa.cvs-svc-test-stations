eb2b6406827257fad70077f7af00a06b859c93d1
Bhavana Siddappa Hemanth <bhemanth@deloitte.co.uk>
2019-10-29 16:07:56 +0000

    CVSB-9040: Adding DB setup steps for testStationsFunctions intTest.

--- tests/integration/testStationsFunction.intTest.ts
+++ tests/integration/testStationsFunction.intTest.ts
@@ -4,53 +4,92 @@ import { getTestStationsEmails } from "../../src/functions/getTestStationsEmails
 import {ITestStation} from "../../src/models/ITestStation";
 import {HTTPError} from "../../src/models/HTTPError";
 import {HTTPResponse} from "../../src/models/HTTPResponse";
+import {emptyDatabase, populateDatabase} from "../util/dbOperations";
 
 describe("getTestStations", () => {
-  it("should return a promise", () => {
-    return LambdaTester(getTestStations)
-      .expectResolve((result: any) => {
-        expect(result).toBeTruthy();
-      });
-  });
+    beforeEach(async () => {
+        await emptyDatabase();
+        await populateDatabase();
+    });
 
-  it("should return only the active test stations", () => {
-    return LambdaTester(getTestStations)
-      .expectResolve((result: any) => {
-        expect(result).toBeTruthy();
-        expect((result as HTTPResponse).statusCode).toEqual(200);
-      });
-  });
+    afterEach(async () => {
+        await emptyDatabase();
+    });
+
+    beforeAll(async () => {
+        await emptyDatabase();
+        await populateDatabase();
+    });
+
+    afterAll(async () => {
+        await emptyDatabase();
+    });
+
+    context("when database is populated", () => {
+        it("should return a promise", () => {
+            return LambdaTester(getTestStations)
+                .expectResolve((result: any) => {
+                    expect(result).toBeTruthy();
+                });
+        });
+
+        it("should return only the active test stations", () => {
+            return LambdaTester(getTestStations)
+                .expectResolve((result: any) => {
+                    expect(result).toBeTruthy();
+                    expect((result as HTTPResponse).statusCode).toEqual(200);
+                });
+        });
+    });
 });
 
 describe("getTestStationsEmail", () => {
-  it("should return an error when sending no parameters", () => {
-    return LambdaTester(getTestStationsEmails)
-        .expectReject((error: Error) => {
-          expect(error).toBeTruthy();
-          expect(error).toBeInstanceOf(HTTPError);
-          expect((error as HTTPError).statusCode).toEqual(400);
-        });
-  });
-  it("should return a promise when sending parameters", () => {
-    return LambdaTester(getTestStationsEmails)
-        .event({
-            pathParameters: {
-            testStationPNumber: "87-1369569" }
-        })
-        .expectResolve((result: ITestStation[]) => {
-          expect(result).toBeTruthy();
-        });
-  });
-  it("should throw and error when requesting non-existent record", () => {
-    return LambdaTester(getTestStationsEmails)
-        .event({
-          pathParameters: {
-            testStationPNumber: "111" }
-        })
-        .expectReject((error: Error) => {
-          expect(error).toBeTruthy();
-          expect(error).toBeInstanceOf(HTTPError);
-          expect((error as HTTPError).statusCode).toEqual(404);
-        });
-  });
+    beforeEach(async () => {
+        await emptyDatabase();
+        await populateDatabase();
+    });
+
+    afterEach(async () => {
+        await emptyDatabase();
+    });
+
+    beforeAll(async () => {
+        await emptyDatabase();
+        await populateDatabase();
+    });
+
+    afterAll(async () => {
+        await emptyDatabase();
+    });
+
+      it("should return an error when sending no parameters", () => {
+        return LambdaTester(getTestStationsEmails)
+            .expectReject((error: Error) => {
+              expect(error).toBeTruthy();
+              expect(error).toBeInstanceOf(HTTPError);
+              expect((error as HTTPError).statusCode).toEqual(400);
+            });
+      });
+      it("should return a promise when sending parameters", () => {
+        return LambdaTester(getTestStationsEmails)
+            .event({
+                pathParameters: {
+                testStationPNumber: "87-1369569" }
+            })
+            .expectResolve((result: ITestStation[]) => {
+              expect(result).toBeTruthy();
+            });
+      });
+      it("should throw and error when requesting non-existent record", () => {
+        return LambdaTester(getTestStationsEmails)
+            .event({
+              pathParameters: {
+                testStationPNumber: "111" }
+            })
+            .expectReject((error: Error) => {
+              expect(error).toBeTruthy();
+              expect(error).toBeInstanceOf(HTTPError);
+              expect((error as HTTPError).statusCode).toEqual(404);
+            });
+      });
 });
