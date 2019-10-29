ecdd5cd92e3af2bfffadc261b886c2d4adc0854d
Bhavana Siddappa Hemanth <bhemanth@deloitte.co.uk>
2019-10-29 10:04:32 +0000

    CVSB-9040: Updating dbOperation file

--- tests/util/dbOperations.ts
+++ tests/util/dbOperations.ts
@@ -1,29 +1,27 @@
-import * as fs from "fs";
-import * as path from "path";
 import { TestStationDAO } from "../../src/models/TestStationDAO";
-import { ITestStation } from "../../src/models/ITestStation";
-import { TestStationService } from "../../src/services/TestStationService";
+import testStations from "../resources/test-stations.json";
+import * as _ from "lodash";
 
-export const populateDatabase = () => {
-    const testStationMockDB = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../resources/test-stations.json"), "utf8"));
-    const testStationService = new TestStationService(new TestStationDAO());
-    testStationService.insertTestStationList(testStationMockDB);
+export const populateDatabase = async () => {
+    const mockBuffer = _.cloneDeep(testStations);
+    const testStationDAO = new TestStationDAO();
+    const batches = [];
+    while (mockBuffer.length > 0) {
+        batches.push(mockBuffer.splice(0, 25));
+    }
+    for (const batch of batches) {
+        await testStationDAO.createMultiple(batch);
+    }
 };
 
-export const emptyDatabase = () => {
+export const emptyDatabase = async() => {
     const testStationDAO = new TestStationDAO();
-    const dataBuffer = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../resources/test-stations.json"), "utf8"));
-
+    const mockBuffer = _.cloneDeep(testStations).map((station) => station.testStationId);
     const batches = [];
-    while (dataBuffer.length > 0) {
-        batches.push(dataBuffer.splice(0, 25));
+    while (mockBuffer.length > 0) {
+        batches.push(mockBuffer.splice(0, 25));
     }
-
-    batches.forEach((batch) => {
-        testStationDAO.deleteMultiple(
-            batch.map((item: any) => {
-                return item.testStationId;
-            })
-        );
-    });
+    for (const batch of batches) {
+        await testStationDAO.deleteMultiple(batch);
+    }
 };
