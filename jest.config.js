2dcd542ab3716b0e8e09d7c88e36c042972a7e7c
Ian-Hodges <ian.hodges@bjss.com>
2022-02-18 15:44:50 +0000

    fix(cb2-2784): fix sonarqube pipeline coverage

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,17 @@ module.exports = {
   testMatch: ["**/*.unitTest.ts"],
   setupFiles: ["jest-plugin-context/setup"],
   moduleFileExtensions: ["js", "ts"],
-  coverageDirectory: "./coverage",
+  coverageDirectory: "coverage",
   collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,ts}', '!**/node_modules/**'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
   testResultsProcessor: "jest-sonar-reporter",
   testURL: "http://localhost",
   transform: {
